fdcad0ea8dafc3c7085bcff671c0217f8ff04454
cc <cc@niseitekimacbook-pro.local>
2018-07-25 10:30:41 +0800

    Initial Commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by cc on 2018/7/25.
+//  Copyright © 2018年 qingpugonglusuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by cc on 2018/7/25.
+//  Copyright © 2018年 qingpugonglusuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
